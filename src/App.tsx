9ab7f59335942580a3af24f81db1902f7fa85d3c
Robert Williamson <robert11122002@gmail.com>
2023-06-24 13:26:36 +0100

    done list

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,19 @@ function App() {
     return JSON.parse(list);
   });
 
+  const [done, updateDone] = useState(() => {
+    const list = localStorage.getItem("TODO_DONE");
+    if (list == null) return [];
+    return JSON.parse(list);
+  });
+
   const [newItem, updateNewItem] = useState("");
 
   useEffect(() => {
     localStorage.setItem("ITEMS", JSON.stringify(todos));
+  });
+  useEffect(() => {
+    localStorage.setItem("TODO_DONE", JSON.stringify(done));
   });
 
   const addTodo = (e: MouseEvent) => {
@@ -39,6 +48,20 @@ function App() {
     });
   };
 
+  const addDone = function (doneTodo: TodoItem) {
+    updateDone((currentDone: TodoItem[]) => {
+      return [...currentDone, doneTodo];
+    });
+  };
+
+  const deleteDone = (id: string) => {
+    updateDone((done: TodoItem[]) => {
+      return done.filter((todo: TodoItem) => {
+        return todo.id !== id;
+      });
+    });
+  };
+
   const deleteTodo = (id: string) => {
     setTodo((todos: TodoItem[]) => {
       return todos.filter((todo: TodoItem) => {
@@ -52,29 +75,69 @@ function App() {
       <div className="todo-app">
         <TodoForm itemAdd={addTodo} content={newItem} update={updateNewItem} />
 
-        <div className="todo-container">
-          {!todos.length && <h1>No TODOs found.</h1>}
-          {todos.map((todo: TodoItem) => {
-            //<Todo title={todo.what} completed={todo.completed} />;
-            return (
-              <li key={todo.id} className="todo-item">
-                <label className="todo-content">
-                  <input
-                    type="checkbox"
-                    checked={todo.completed}
-                    onChange={(e) => handleButton(todo.id, e.target.checked)}
-                  />
-                  {todo.what}
-                </label>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => deleteTodo(todo.id)}
-                >
-                  Delete
-                </button>
-              </li>
-            );
-          })}
+        <div className="done-undone">
+          <div className="todo-container">
+            <p className="todo-info">Todos</p>
+            {!todos.length && (
+              <h1 className="no-todo-found">No TODOs found.</h1>
+            )}
+            {todos.map((todo: TodoItem) => {
+              //<Todo title={todo.what} completed={todo.completed} />;
+              return (
+                <li key={todo.id} className="todo-item">
+                  <label className="todo-content">
+                    <input
+                      type="checkbox"
+                      checked={todo.completed}
+                      onChange={(e) => handleButton(todo.id, e.target.checked)}
+                    />
+                    {todo.what}
+                  </label>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteTodo(todo.id)}
+                  >
+                    Delete
+                  </button>
+                  {todo.completed && (
+                    <button
+                      className="btn btn-success"
+                      onClick={() => {
+                        deleteTodo(todo.id);
+                        addDone(todo);
+                      }}
+                    >
+                      Mark Done
+                    </button>
+                  )}
+                </li>
+              );
+            })}
+          </div>
+
+          <div className="done-container">
+            <p className="todo-info">Done Todos</p>
+            {done.map((todo: TodoItem) => {
+              return (
+                <li key={todo.id} className="done-item">
+                  <label className="done-content">
+                    {/* <input
+                      type="checkbox"
+                      checked={todo.completed}
+                      onChange={(e) => handleButton(todo.id, e.target.checked)}
+                    /> */}
+                    {todo.what}
+                  </label>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteDone(todo.id)}
+                  >
+                    Oops!
+                  </button>
+                </li>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
